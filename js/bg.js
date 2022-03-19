7b74682cc0ee98671697cd15e213de69929f40b4
yoon_z <yoon_z@iyunjiui-MacBook-Pro.local>
2022-03-19 20:04:27 +0900

    bg오류 수정

--- js/bg.js
+++ js/bg.js
@@ -1,7 +1,11 @@
 const bgClass = document.querySelector('.bg');
+const loginBgClass = document.querySelector('.login-bg');
 
 const imgArr = ['00.jpeg', '01.jpg', '02.jpg'];
-const chosenImg = imgArr[Math.floor(Math.random()*imgArr.length)];
-const bgImg = document.createElement('img');
-bgImg.src = `img/${chosenImg}`;
-bgClass.appendChild(bgImg);
+const randomImg = imgArr[Math.floor(Math.random()*imgArr.length)];
+// const bgImg = document.createElement('img');
+bgClass.style.backgroundImage = `url(../img/${randomImg})`
+loginBgClass.style.backgroundImage = `url(../img/${randomImg})`
+// bgImg.src = `img/${randomImg}`;
+// bgClass.appendChild(bgImg);
+// loginBgClass.appendChild(bgImg);
