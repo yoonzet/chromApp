7a697f704fa0bf5bc66510c944c669c8e76096f8
yoonji lee <lalala703@naver.com>
2022-03-19 22:39:34 +0900

    bg

--- js/bg.js
+++ js/bg.js
@@ -4,5 +4,5 @@ const loginBgClass = document.querySelector('.login-bg');
 const imgArr = ['00.jpeg', '01.jpg', '02.jpg'];
 const randomImg = imgArr[Math.floor(Math.random()*imgArr.length)];
 
-bgClass.style.backgroundImage = `url(./img/${randomImg})`;
-loginBgClass.style.backgroundImage = `url(./img/${randomImg})`;
+bgClass.style.backgroundImage = `url('img/${randomImg}')`;
+loginBgClass.style.backgroundImage = `url('img/${randomImg}')`;
