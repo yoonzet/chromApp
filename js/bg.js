9035d2033d30d8293e9677298d7631a5e55adaa6
yoonji lee <lalala703@naver.com>
2022-03-19 22:24:13 +0900

    재수정

--- js/bg.js
+++ js/bg.js
@@ -3,9 +3,6 @@ const loginBgClass = document.querySelector('.login-bg');
 
 const imgArr = ['00.jpeg', '01.jpg', '02.jpg'];
 const randomImg = imgArr[Math.floor(Math.random()*imgArr.length)];
-// const bgImg = document.createElement('img');
-bgClass.style.backgroundImage = `url(img/${randomImg})`
-loginBgClass.style.backgroundImage = `url(img/${randomImg})`
-// bgImg.src = `img/${randomImg}`;
-// bgClass.appendChild(bgImg);
-// loginBgClass.appendChild(bgImg);
+
+bgClass.style.backgroundImage = `url(../img/${randomImg})`;
+loginBgClass.style.backgroundImage = `url(../img/${randomImg})`;
